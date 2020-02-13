9731674279a0771ba5306d7feb72517c2567744b
Spencer Curtis <spencercolecurtis@gmail.com>
2020-02-13 14:13:16 -0700

    Add starter project

---
+++ TimeTracker/Resources/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TimeTracker
+//
+//  Created by Spencer Curtis on 2/11/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ TimeTracker/Resources/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  TimeTracker
+//
+//  Created by Spencer Curtis on 2/11/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
