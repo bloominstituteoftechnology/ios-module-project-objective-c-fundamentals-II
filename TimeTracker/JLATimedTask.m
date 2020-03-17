97385a437d59b81d98315c906a6fcf074a6bf8b6
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-03-17 13:13:00 -0700

    Part 1 done

--- TimeTracker/JLATimedTask.m
+++ TimeTracker/JLATimedTask.m
@@ -10,4 +10,24 @@
 
 @implementation JLATimedTask
 
+- (instancetype)initWithClientName:(NSString *)clientName
+                           summary:(NSString *)summary
+                        hourlyRate:(double)hourlyRate
+                       hoursWorked:(double)hoursWorked {
+    
+    self = [super init];
+    if (self) {
+        
+        _clientName = clientName;
+        _summary = summary;
+        _hourlyRate = hourlyRate;
+        _hoursWorked = hoursWorked;
+    }
+    return self;
+}
+
+- (double)total {
+    return _hoursWorked * _hourlyRate;
+}
+
 @end
