605f6fce0aeddda283ec83fa09440c5db7243167
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-03-17 12:39:02 -0700

    added timedtask model

---
+++ TimeTracker/JLATimedTask.m
@@ -0,0 +1,13 @@
+//
+//  JLATimedTask.m
+//  TimeTracker
+//
+//  Created by Jorge Alvarez on 3/17/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "JLATimedTask.h"
+
+@implementation JLATimedTask
+
+@end
