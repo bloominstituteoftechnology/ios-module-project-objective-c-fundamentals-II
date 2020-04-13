02c4d6e6af9e5801dd36f664fe2562be78883bf9
dillonp23 <dillonp.lambda@gmail.com>
2020-04-12 20:38:29 -0700

    Added method functionality to insert new timed task. Added debug method to fill timed task array with test data.

--- TimeTracker/TTATimedTaskController.m
+++ TimeTracker/TTATimedTaskController.m
@@ -7,6 +7,7 @@
 //
 
 #import "TTATimedTaskController.h"
+#import "TTATimeTracker.h"
 
 @interface TTATimedTaskController () {
     NSMutableArray *_internalTimedTask;
@@ -21,8 +22,27 @@
 {
     if (self = [super init]) {
         _internalTimedTask = [[NSMutableArray alloc] init];
+
+        
+#ifdef DEBUG
+        [self addTestData];
     }
+#endif
+    
     return self;
+}
+
+- (void)addTestData
+{
+    [_internalTimedTask addObject:[[TTATimeTracker alloc] initWithClient:@"Client"
+                                                                 summary:@"No summary"
+                                                              hourlyRate:20.0
+                                                           numberOfHours:2.0]];
+    
+    [_internalTimedTask addObject:[[TTATimeTracker alloc] initWithClient:@"Client 2"
+                                                                 summary:@"No summary here"
+                                                              hourlyRate:25.0
+                                                           numberOfHours:3.0]];
 }
 
 - (NSArray<TTATimeTracker *> *)timedTasks
@@ -33,6 +53,16 @@
 - (NSUInteger)timedTasksCount
 {
     return _internalTimedTask.count;
+}
+
+- (TTATimeTracker *)taskAtIndex:(NSUInteger)index
+{
+    return [_internalTimedTask objectAtIndex:index];
+}
+
+- (void)addTimedTask:(TTATimeTracker *)aTimedTask
+{
+    [_internalTimedTask addObject:aTimedTask];
 }
 
 @end
