6e32bbed1e13360c52571d48b7b1c017a8dde9d0
Elizabeth Wingate <wingateep@icloud.com>
2020-04-14 18:44:14 -0400

    Part 1 -TimedTask

---
+++ TimeTracker/Storyboards/TimedTask.h
@@ -0,0 +1,28 @@
+//
+//  TimedTask.h
+//  TimeTracker
+//
+//  Created by beth on 4/14/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TimedTask : NSObject
+
+@property (nonatomic) NSString *client;
+@property (nonatomic) NSString *workSummary;
+@property (nonatomic) double hourlyRate;
+@property (nonatomic) double hoursWorked;
+@property (nonatomic, readonly) double total;
+
+- (instancetype) initWithClient:(NSString *)client
+                        summary:(NSString *)summary
+                           rate:(double)rate
+                    hoursWorked:(double)hoursWorked;
+
+@end
+
+NS_ASSUME_NONNULL_END
