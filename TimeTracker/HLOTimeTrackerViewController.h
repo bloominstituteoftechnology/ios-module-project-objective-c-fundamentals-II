6121eda2e3ff71cf6e405f958617e7565494b796
Hector L <hectoremil21@hotmail.com>
2020-05-11 17:42:24 -0400

    Create view controller.

---
+++ TimeTracker/HLOTimeTrackerViewController.h
@@ -0,0 +1,17 @@
+//
+//  HLOTimeTrackerViewController.h
+//  TimeTracker
+//
+//  Created by Karen Rodriguez on 5/11/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HLOTimeTrackerViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TimeTracker/HLOTimeTrackerViewController.m
@@ -0,0 +1,32 @@
+//
+//  HLOTimeTrackerViewController.m
+//  TimeTracker
+//
+//  Created by Karen Rodriguez on 5/11/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "HLOTimeTrackerViewController.h"
+
+@interface HLOTimeTrackerViewController () <UITableViewDataSource>
+
+@end
+
+@implementation HLOTimeTrackerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
