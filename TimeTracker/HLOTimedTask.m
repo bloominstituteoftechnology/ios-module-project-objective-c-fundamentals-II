348d0e60c72ec6e854fe5cb92b5e150c29a1b987
Hector L <hectoremil21@hotmail.com>
2020-05-11 16:54:24 -0400

    Create TimedTask NSObject files.

---
+++ TimeTracker/HLOTimedTask.m
@@ -0,0 +1,13 @@
+//
+//  HLOTimedTask.m
+//  TimeTracker
+//
+//  Created by Karen Rodriguez on 5/11/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "HLOTimedTask.h"
+
+@implementation HLOTimedTask
+
+@end
