a8c00120e941d97804e6cd613137f7f3d4790dcf
Hector L <hectoremil21@hotmail.com>
2020-05-11 17:12:12 -0400

    Create TimedTaskController NSObject class files

---
+++ TimeTracker/HLOTimedTaskController.h
@@ -0,0 +1,17 @@
+//
+//  HLOTimedTaskController.h
+//  TimeTracker
+//
+//  Created by Karen Rodriguez on 5/11/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HLOTimedTaskController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TimeTracker/HLOTimedTaskController.m
@@ -0,0 +1,13 @@
+//
+//  HLOTimedTaskController.m
+//  TimeTracker
+//
+//  Created by Karen Rodriguez on 5/11/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "HLOTimedTaskController.h"
+
+@implementation HLOTimedTaskController
+
+@end
