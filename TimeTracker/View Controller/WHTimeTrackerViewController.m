4dd2a0d6037ea241bf3171fda679da40f8805d6d
Wyatt Harrell <wyattaharrell@gmail.com>
2020-05-12 16:52:14 -0400

    Reorganized files into respective folders

---
+++ TimeTracker/View Controller/WHTimeTrackerViewController.m
@@ -0,0 +1,32 @@
+//
+//  WHTimeTrackerViewController.m
+//  TimeTracker
+//
+//  Created by Wyatt Harrell on 5/12/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "WHTimeTrackerViewController.h"
+
+@interface WHTimeTrackerViewController ()
+
+@end
+
+@implementation WHTimeTrackerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
