97bb50047cac7b3d9af2539e9b3c1a2cf5cbe754
Tobi Tselios <tobi.tselios@gmail.com>
2020-05-12 23:17:26 +0100

    Callied the Basic Initialiser in the Implementation File and Implemented the 'createTimedTaskWith' Method

--- TimeTracker/OTKTimedTaskController.m
+++ TimeTracker/OTKTimedTaskController.m
@@ -7,7 +7,28 @@
 //
 
 #import "OTKTimedTaskController.h"
+#import "OTKTimedTask.h"
 
 @implementation OTKTimedTaskController
+
+- (instancetype)init {
+    if (self = [super init]) {
+        _timedTasks = [[NSMutableArray alloc] init];
+    }
+
+    return self;
+}
+
+- (void)createTimedTasksWithClient:(NSString *)client
+                       workSummary:(NSString *)workSummary
+                        hourlyRate:(int)hourlyRate
+                       hoursWorked:(int)hoursWorked {
+
+    OTKTimedTask *timedTask = [[OTKTimedTask alloc] initWithClient:client
+                                                       workSummary:workSummary
+                                                        hourlyRate:hourlyRate
+                                                       hoursWorked:hoursWorked];
+    [self.timedTasks addObject:timedTask];
+}
 
 @end
