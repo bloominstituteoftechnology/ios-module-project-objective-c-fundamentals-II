cb1d258a83d95984c9d5a2cbc758cc4acc828780
Shawn Gee <shawngee@gmail.com>
2020-05-12 15:56:10 -0400

    Added Timed Task and stubbed out interface

---
+++ TimeTracker/Model/SSTimedTask.m
@@ -0,0 +1,13 @@
+//
+//  SSTimedTask.m
+//  TimeTracker
+//
+//  Created by Shawn Gee on 5/12/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "SSTimedTask.h"
+
+@implementation SSTimedTask
+
+@end
