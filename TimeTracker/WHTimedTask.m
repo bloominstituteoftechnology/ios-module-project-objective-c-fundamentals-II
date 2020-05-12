c15b6cc9b1a41137dabbbb999c31edaddd6accb6
Wyatt Harrell <wyattaharrell@gmail.com>
2020-05-12 16:22:40 -0400

    TimedTask's header file complete

---
+++ TimeTracker/WHTimedTask.m
@@ -0,0 +1,13 @@
+//
+//  WHTimedTask.m
+//  TimeTracker
+//
+//  Created by Wyatt Harrell on 5/12/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "WHTimedTask.h"
+
+@implementation WHTimedTask
+
+@end
