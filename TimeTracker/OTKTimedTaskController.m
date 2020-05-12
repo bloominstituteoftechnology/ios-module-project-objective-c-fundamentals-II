3e307f2cf4d64a278759f105397a70dc891799b8
Tobi Tselios <tobi.tselios@gmail.com>
2020-05-12 23:02:16 +0100

    TimedTaskController Header File Setup

---
+++ TimeTracker/OTKTimedTaskController.m
@@ -0,0 +1,13 @@
+//
+//  OTKTimedTaskController.m
+//  TimeTracker
+//
+//  Created by Tobi Kuyoro on 12/05/2020.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "OTKTimedTaskController.h"
+
+@implementation OTKTimedTaskController
+
+@end
