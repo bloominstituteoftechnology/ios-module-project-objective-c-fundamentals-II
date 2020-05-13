aa4dc2e914da1edbd244e2741f2db3f74614e95e
Mark Gerrior <github@gerrior.com>
2020-05-12 18:09:40 -0700

    Added MTGTimedTask

---
+++ TimeTracker/MTGTimedTask.m
@@ -0,0 +1,13 @@
+//
+//  MTGTimedTask.m
+//  TimeTracker
+//
+//  Created by Mark Gerrior on 5/12/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "MTGTimedTask.h"
+
+@implementation MTGTimedTask
+
+@end
