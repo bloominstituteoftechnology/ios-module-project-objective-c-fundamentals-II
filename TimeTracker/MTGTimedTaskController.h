b3079030ddbf442d51a455506b60d97f9c3893d6
Mark Gerrior <github@gerrior.com>
2020-05-12 22:56:23 -0700

    Added MTGTimedTaskController object

---
+++ TimeTracker/MTGTimedTaskController.h
@@ -0,0 +1,17 @@
+//
+//  MTGTimedTaskController.h
+//  TimeTracker
+//
+//  Created by Mark Gerrior on 5/12/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MTGTimedTaskController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TimeTracker/MTGTimedTaskController.m
@@ -0,0 +1,13 @@
+//
+//  MTGTimedTaskController.m
+//  TimeTracker
+//
+//  Created by Mark Gerrior on 5/12/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "MTGTimedTaskController.h"
+
+@implementation MTGTimedTaskController
+
+@end
