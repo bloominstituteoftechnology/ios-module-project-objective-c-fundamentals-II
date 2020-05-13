da060109fc800a3188cf22e944b8053e3dfc4438
Mark Gerrior <github@gerrior.com>
2020-05-12 23:11:48 -0700

    Added MTGTimeTrackerViewController files

---
+++ TimeTracker/MTGTimeTrackerViewController.h
@@ -0,0 +1,17 @@
+//
+//  MTGTimeTrackerViewController.h
+//  TimeTracker
+//
+//  Created by Mark Gerrior on 5/12/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MTGTimeTrackerViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TimeTracker/MTGTimeTrackerViewController.m
@@ -0,0 +1,32 @@
+//
+//  MTGTimeTrackerViewController.m
+//  TimeTracker
+//
+//  Created by Mark Gerrior on 5/12/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "MTGTimeTrackerViewController.h"
+
+@interface MTGTimeTrackerViewController ()
+
+@end
+
+@implementation MTGTimeTrackerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
