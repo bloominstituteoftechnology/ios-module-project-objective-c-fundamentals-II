c619b2b0bc7d54b856b751b56c76af3d01ed9bd7
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-05-22 11:43:24 -0400

    Created TimeTrackerViewController

---
+++ TimeTracker/SKITimeTrackerViewController.h
@@ -0,0 +1,17 @@
+//
+//  SKITimeTrackerViewController.h
+//  TimeTracker
+//
+//  Created by Joshua Rutkowski on 5/22/20.
+//  Copyright © 2020 Rutkowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SKITimeTrackerViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TimeTracker/SKITimeTrackerViewController.m
@@ -0,0 +1,32 @@
+//
+//  SKITimeTrackerViewController.m
+//  TimeTracker
+//
+//  Created by Joshua Rutkowski on 5/22/20.
+//  Copyright © 2020 Rutkowski. All rights reserved.
+//
+
+#import "SKITimeTrackerViewController.h"
+
+@interface SKITimeTrackerViewController ()
+
+@end
+
+@implementation SKITimeTrackerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
