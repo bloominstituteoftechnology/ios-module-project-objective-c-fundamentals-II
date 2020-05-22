485527f5856645d86c75fbade1226d43f707865b
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-05-22 10:56:24 -0400

    Created model controller

---
+++ TimeTracker/Model Controller/SKITimedTaskController.h
@@ -0,0 +1,17 @@
+//
+//  SKITimedTaskController.h
+//  TimeTracker
+//
+//  Created by Joshua Rutkowski on 5/22/20.
+//  Copyright © 2020 Rutkowski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SKITimedTaskController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TimeTracker/Model Controller/SKITimedTaskController.m
@@ -0,0 +1,13 @@
+//
+//  SKITimedTaskController.m
+//  TimeTracker
+//
+//  Created by Joshua Rutkowski on 5/22/20.
+//  Copyright © 2020 Rutkowski. All rights reserved.
+//
+
+#import "SKITimedTaskController.h"
+
+@implementation SKITimedTaskController
+
+@end
