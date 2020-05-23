75254cce1eb9167c93fd975fab87d1715ece1b32
Sal <salmanamer@gmail.com>
2020-05-22 21:45:56 -0700

    created timetaskController

---
+++ TimeTracker/TTRTimedTaskController.m
@@ -0,0 +1,13 @@
+//
+//  TTRTimedTaskController.m
+//  TimeTracker
+//
+//  Created by Sal B Amer on 5/22/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "TTRTimedTaskController.h"
+
+@implementation TTRTimedTaskController
+
+@end
