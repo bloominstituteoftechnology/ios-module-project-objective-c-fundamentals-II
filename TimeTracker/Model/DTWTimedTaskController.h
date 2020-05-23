f72b7968705e7ba9592a7c2ef6c62a2c9ea8d6a6
David Wright <davidtwright89@gmail.com>
2020-05-23 19:05:35 -0400

    Created the header and ".m" files for a new "DTWTimedTaskController" object

---
+++ TimeTracker/Model/DTWTimedTaskController.h
@@ -0,0 +1,17 @@
+//
+//  DTWTimedTaskController.h
+//  TimeTracker
+//
+//  Created by David Wright on 5/23/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DTWTimedTaskController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TimeTracker/Model/DTWTimedTaskController.m
@@ -0,0 +1,13 @@
+//
+//  DTWTimedTaskController.m
+//  TimeTracker
+//
+//  Created by David Wright on 5/23/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "DTWTimedTaskController.h"
+
+@implementation DTWTimedTaskController
+
+@end
