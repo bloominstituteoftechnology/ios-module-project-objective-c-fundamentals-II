27666c205221ce36db3a2c352743f9d738b2dfa3
Sal <salmanamer@gmail.com>
2020-05-22 21:17:02 -0700

    setup project and TimedTask file

---
+++ TimeTracker/TTRTimedTask.h
@@ -0,0 +1,17 @@
+//
+//  TTRTimedTask.h
+//  TimeTracker
+//
+//  Created by Sal B Amer on 5/22/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TTRTimedTask : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TimeTracker/TTRTimedTask.m
@@ -0,0 +1,13 @@
+//
+//  TTRTimedTask.m
+//  TimeTracker
+//
+//  Created by Sal B Amer on 5/22/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "TTRTimedTask.h"
+
+@implementation TTRTimedTask
+
+@end
