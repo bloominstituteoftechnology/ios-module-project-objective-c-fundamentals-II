88c6416acb043ca1e90b04f0aa6fbc777f9b4175
Jessie <jessie.griffin@icloud.com>
2020-05-22 21:39:45 -0400

    Adds JAGTimeTrackerViewController files

---
+++ TimeTracker/View Controller/JAGTimeTrackerViewController.h
@@ -0,0 +1,17 @@
+//
+//  JAGTimeTrackerViewController.h
+//  TimeTracker
+//
+//  Created by Jessie Ann Griffin on 5/22/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JAGTimeTrackerViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
