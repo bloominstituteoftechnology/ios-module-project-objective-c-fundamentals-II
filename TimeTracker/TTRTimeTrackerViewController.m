7c2e486799fc169c62ec9218b88bfad66a4db9c3
Sal <salmanamer@gmail.com>
2020-05-22 23:52:36 -0700

    Created TTRTimeTrackerViewController
    #import "TTRTimeTrackerViewController.h"
    #import "TTRTimedTaskController.h"
    #import "TTRTimedTask.h"

---
+++ TimeTracker/TTRTimeTrackerViewController.m
@@ -0,0 +1,34 @@
+//
+//  TTRTimeTrackerViewController.m
+//  TimeTracker
+//
+//  Created by Sal B Amer on 5/22/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "TTRTimeTrackerViewController.h"
+#import "TTRTimedTaskController.h"
+#import "TTRTimedTask.h"
+
+@interface TTRTimeTrackerViewController ()
+
+@end
+
+@implementation TTRTimeTrackerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
