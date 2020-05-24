f585f06d3b166687e698103ffd85801405ef48b3
David Wright <davidtwright89@gmail.com>
2020-05-24 02:58:00 -0400

    Deleted & re-added DTWTimeTrackerViewController files because Xcode could not locate them in the project folder

---
+++ TimeTracker/DTWTimeTrackerViewController.h
@@ -0,0 +1,17 @@
+//
+//  DTWTimeTrackerViewController.h
+//  TimeTracker
+//
+//  Created by David Wright on 5/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DTWTimeTrackerViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TimeTracker/DTWTimeTrackerViewController.m
@@ -0,0 +1,32 @@
+//
+//  DTWTimeTrackerViewController.m
+//  TimeTracker
+//
+//  Created by David Wright on 5/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "DTWTimeTrackerViewController.h"
+
+@interface DTWTimeTrackerViewController ()
+
+@end
+
+@implementation DTWTimeTrackerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
