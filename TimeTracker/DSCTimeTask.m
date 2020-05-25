01f6ad8ba89e6e9e853d61cf5c6d67fcf11b5609
Denis Cedeno <denis.cedeno@gmail.com>
2020-05-25 09:55:45 -0400

    part 1: created time tracker implementation file with initializer and getter method for total

--- TimeTracker/DSCTimeTask.m
+++ TimeTracker/DSCTimeTask.m
@@ -10,4 +10,20 @@
 
 @implementation DSCTimeTask
 
+- (instancetype)initWithClient:(NSString *)aClient
+                   workSummary:(NSString *)aSummaryOfWork rateCharged:(double)aRateCharged hoursWorked:(double)aWorkedHours
+{
+    if (self = [super init]) {
+        _client = aClient.copy;
+        _workSummary = aSummaryOfWork.copy;
+        _rateCharged = aRateCharged;
+        _hoursWorked = aWorkedHours;
+    }
+    return self;
+}
+
+- (double)total {
+    return self.hoursWorked * self.rateCharged;
+}
+
 @end
