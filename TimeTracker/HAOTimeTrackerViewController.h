a42e733162341ed7a871b6908fb2689dffcd4210
Hunter Oppel <hunter.oppel@gmail.com>
2020-06-09 18:48:52 -0400

    Added view controller

---
+++ TimeTracker/HAOTimeTrackerViewController.h
@@ -0,0 +1,17 @@
+//
+//  HAOTimeTrackerViewController.h
+//  TimeTracker
+//
+//  Created by Hunter Oppel on 6/9/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HAOTimeTrackerViewController : UIViewController <UITableViewDataSource>
+
+@end
+
+NS_ASSUME_NONNULL_END
