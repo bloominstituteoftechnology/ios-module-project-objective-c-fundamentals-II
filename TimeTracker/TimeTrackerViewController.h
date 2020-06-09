453007ca9834431d1468d66c52afaf202764e325
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-06-09 14:38:27 -0700

    Added the property for timedtaskcontroller

--- TimeTracker/TimeTrackerViewController.h
+++ TimeTracker/TimeTrackerViewController.h
@@ -7,10 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
-
+@class TimedTaskController;
 NS_ASSUME_NONNULL_BEGIN
 
 @interface TimeTrackerViewController : UIViewController
+
+@property (nonatomic) TimedTaskController *timeTaskController;
 
 @end
 
