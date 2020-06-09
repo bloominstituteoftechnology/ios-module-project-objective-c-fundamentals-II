a140236d3b244e9450ec0d23b71294fe05ef60f1
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-06-09 13:49:06 -0700

    Added NSObject TimedTask

---
+++ TimeTracker/LSITimeTask.h
@@ -0,0 +1,17 @@
+//
+//  LSITimeTask.h
+//  TimeTracker
+//
+//  Created by Bhawnish Kumar on 6/9/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSITimeTask : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TimeTracker/LSITimeTask.m
@@ -0,0 +1,13 @@
+//
+//  LSITimeTask.m
+//  TimeTracker
+//
+//  Created by Bhawnish Kumar on 6/9/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "LSITimeTask.h"
+
+@implementation LSITimeTask
+
+@end
