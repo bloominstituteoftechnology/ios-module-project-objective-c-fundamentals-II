00d9476dd856098986ad484aa6099d820ba3480b
Chris Dobek <dobek.christopher@gmail.com>
2020-06-09 19:04:33 -0400

    MVP

---
+++ TimeTracker/CMDTimeTrackerViewController.h
@@ -0,0 +1,19 @@
+//
+//  CMDTimeTrackerViewController.h
+//  TimeTracker
+//
+//  Created by Chris Dobek on 6/9/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class CMDTimedTaskController;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CMDTimeTrackerViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
+
+NS_ASSUME_NONNULL_END
