ddec4f0fd848d30c27f0ed68901674e6733be2a2
Aaron Haughton <dreamcast2000@outlook.com>
2020-06-13 12:44:55 -0400

    Wired up IBOutlets and IBActions

--- TimeTracker/Models/LSITimeTracker.h
+++ TimeTracker/Models/LSITimeTracker.h
@@ -12,6 +12,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface LSITimeTracker : NSObject
 
+
 @property (nonatomic, copy) NSString *clientName;
 @property (nonatomic) NSString *taskNotes;
 @property (nonatomic) double rate;
