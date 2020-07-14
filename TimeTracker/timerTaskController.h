c59e8c1996d4e560a7b3bb1a48c141ea0bb8e2c3
Kelson Hartle <kelson99hartle@icloud.com>
2020-07-14 15:22:16 -0600

    Set up model

---
+++ TimeTracker/timerTaskController.h
@@ -0,0 +1,27 @@
+//
+//  timerTaskController.h
+//  TimeTracker
+//
+//  Created by Kelson Hartle on 7/14/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface timerTaskController : NSObject
+
+/*
+ We do this because
+ 1- Build projects faster
+ 2- We can reference other classes "recursively"
+ */
+@class LSITimedTask; //foward class declaration (IOU) here's a heads up about a new class
+
+
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TimeTracker/timerTaskController.m
@@ -0,0 +1,17 @@
+//
+//  timerTaskController.m
+//  TimeTracker
+//
+//  Created by Kelson Hartle on 7/14/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "timerTaskController.h"
+
+@implementation timerTaskController
+
+
+
+
+
+@end
