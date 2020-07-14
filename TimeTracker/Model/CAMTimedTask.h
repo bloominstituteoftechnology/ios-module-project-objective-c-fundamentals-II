3aef13b4aaf68e6a3fd8bb58d0be16c66458d2dc
Cody Morley <codymorleyinc@gmail.com>
2020-07-14 14:14:52 -0700

    Part 1 - 4
    Finished the memberwise initializer.

---
+++ TimeTracker/Model/CAMTimedTask.h
@@ -0,0 +1,17 @@
+//
+//  CAMTimedTask.h
+//  TimeTracker
+//
+//  Created by Bling Morley on 7/14/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CAMTimedTask : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TimeTracker/Model/CAMTimedTask.m
@@ -0,0 +1,13 @@
+//
+//  CAMTimedTask.m
+//  TimeTracker
+//
+//  Created by Bling Morley on 7/14/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "CAMTimedTask.h"
+
+@implementation CAMTimedTask
+
+@end
