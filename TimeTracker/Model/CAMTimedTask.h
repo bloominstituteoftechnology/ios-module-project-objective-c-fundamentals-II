3907d5e33103b7cc772c2befae0390ea97608cec
Cody Morley <codymorleyinc@gmail.com>
2020-07-14 14:18:38 -0700

    Part 1 - 5
    Wrote intializer implementation

--- TimeTracker/Model/CAMTimedTask.h
+++ TimeTracker/Model/CAMTimedTask.h
@@ -2,7 +2,7 @@
 //  CAMTimedTask.h
 //  TimeTracker
 //
-//  Created by Bling Morley on 7/14/20.
+//  Created by Cody Morley on 7/14/20.
 //  Copyright © 2020 Lambda School. All rights reserved.
 //
 
@@ -11,6 +11,18 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface CAMTimedTask : NSObject
+
+
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSString *summary;
+@property (nonatomic) double rate;
+@property (nonatomic) double hours;
+@property (nonatomic, readonly) double total;
+
+- (instancetype)initWithName:(NSString *)name
+                     summary:(NSString *)summary
+                        rate:(double)rate
+                       hours:(double)hours;
 
 @end
 
--- TimeTracker/Model/CAMTimedTask.m
+++ TimeTracker/Model/CAMTimedTask.m
@@ -2,12 +2,27 @@
 //  CAMTimedTask.m
 //  TimeTracker
 //
-//  Created by Bling Morley on 7/14/20.
+//  Created by Cody Morley on 7/14/20.
 //  Copyright © 2020 Lambda School. All rights reserved.
 //
 
 #import "CAMTimedTask.h"
 
 @implementation CAMTimedTask
+
+- (instancetype)initWithName:(NSString *)name
+                     summary:(NSString *)summary
+                        rate:(double)rate
+                       hours:(double)hours;
+{
+    self = [super init];
+    if (self) {
+        _name = [name copy];
+        _summary = [summary copy];
+        _rate = rate;
+        _hours = hours;
+    }
+    return self;
+}
 
 @end
