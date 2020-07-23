23d8375f2cde6ac4b4d369fcae045bb30754bbb4
hai666l <0xconr@protonmail.com>
2020-07-23 10:37:40 -0500

    Part 1 - TimedTask - Step 1

---
+++ TimeTracker/XMPTimedTask.h
@@ -0,0 +1,17 @@
+//
+//  XMPTimedTask.h
+//  TimeTracker
+//
+//  Created by conner on 7/23/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XMPTimedTask : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TimeTracker/XMPTimedTask.m
@@ -0,0 +1,13 @@
+//
+//  XMPTimedTask.m
+//  TimeTracker
+//
+//  Created by conner on 7/23/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "XMPTimedTask.h"
+
+@implementation XMPTimedTask
+
+@end
