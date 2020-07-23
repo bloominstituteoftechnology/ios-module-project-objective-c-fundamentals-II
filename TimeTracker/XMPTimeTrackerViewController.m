07df961aa52adbc3fc122ba52546281d4ec9a427
hai666l <0xconr@protonmail.com>
2020-07-23 12:09:13 -0500

    Can now assume the model and controller are working as expected

--- TimeTracker/XMPTimeTrackerViewController.m
+++ TimeTracker/XMPTimeTrackerViewController.m
@@ -8,6 +8,7 @@
 
 #import "XMPTimeTrackerViewController.h"
 #import "XMPTimedTaskController.h"
+#import "XMPTimedTask.h"
 
 @interface XMPTimeTrackerViewController ()
 
@@ -21,6 +22,8 @@
     [controllerTest createTimedTaskWithClientName:@"Joe" summary:@"Paint a fence" hourlyRate:10.00 hoursWorked:4.00];
     [controllerTest createTimedTaskWithClientName:@"Bob" summary:@"Weld a pipe" hourlyRate:15.00 hoursWorked:2.00];
     NSLog(@"timedTasks = %@", controllerTest.timedTasks.description);
+    XMPTimedTask* selectedTask = [controllerTest.timedTasks objectAtIndex:0];
+    NSLog(@"timedTasks[0].clientName = %@", selectedTask.clientName);
 }
 
 @end
