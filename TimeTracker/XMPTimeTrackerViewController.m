c50fe3e43a1565749e6a3181cf6815b24b8d183a
hai666l <0xconr@protonmail.com>
2020-07-23 12:56:05 -0500

    Finished Project Implemented input sanitizing Clear input on task addition

--- TimeTracker/XMPTimeTrackerViewController.m
+++ TimeTracker/XMPTimeTrackerViewController.m
@@ -11,6 +11,7 @@
 #import "XMPTimedTask.h"
 
 @interface XMPTimeTrackerViewController ()
+// MARK: - IBOutlets, Properties
 @property (weak, nonatomic) IBOutlet UITextField *clientTF;
 @property (weak, nonatomic) IBOutlet UITextField *summaryTF;
 @property (weak, nonatomic) IBOutlet UITextField *hourlyRateTF;
@@ -21,24 +22,46 @@
 
 @implementation XMPTimeTrackerViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    tableView.dataSource = self;
-    self.timedTaskController = [[XMPTimedTaskController alloc] init];
-    
-    /*
-    [controllerTest createTimedTaskWithClientName:@"Joe" summary:@"Paint a fence" hourlyRate:10.00 hoursWorked:4.00];
-    [controllerTest createTimedTaskWithClientName:@"Bob" summary:@"Weld a pipe" hourlyRate:15.00 hoursWorked:2.00];
-    NSLog(@"timedTasks = %@", controllerTest.timedTasks.description);
-    XMPTimedTask* selectedTask = [controllerTest.timedTasks objectAtIndex:0];
-    NSLog(@"timedTasks[0].clientName = %@", selectedTask.clientName);
-    */
-    
-    
+// MARK: - IBActions
+- (IBAction)logTime:(UIButton *)sender {
+    if(_clientTF.text.length > 0 && _hourlyRateTF.text.length > 0 && _timeWorkedTF.text.length > 0) {
+        if([_hourlyRateTF.text floatValue] <= 0 || [_timeWorkedTF.text floatValue] <= 0) { return; }
+        // Input sanitized, create task
+        [_timedTaskController
+         createTimedTaskWithClientName:_clientTF.text
+         summary:_summaryTF.text
+         hourlyRate:[_hourlyRateTF.text floatValue]
+         hoursWorked:[_timeWorkedTF.text floatValue]
+        ];
+        
+        // Clear input fields
+        _clientTF.text = @"";
+        _summaryTF.text = @"";
+        _hourlyRateTF.text = @"";
+        _timeWorkedTF.text = @"";
+        
+        [_tableView reloadData];
+    }
 }
 
-- (IBAction)logTime:(UIButton *)sender {
-    
+// MARK: - View Lifecycle
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.tableView.dataSource = self;
+    self.timedTaskController = [[XMPTimedTaskController alloc] init];
+}
+
+// MARK: - TableView Stuff
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return _timedTaskController.timedTasks.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"TaskCell" forIndexPath:indexPath];
+    XMPTimedTask* taskForCell = [self.timedTaskController.timedTasks objectAtIndex:indexPath.row];
+    cell.textLabel.text = taskForCell.clientName;
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"$%.2f", taskForCell.total];
+    return cell;
 }
 
 @end
