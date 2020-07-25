6e63233ba6eca65e41c93af7c607ecae4ea27fa1
Chad Parker <chad@chadparker.com>
2020-07-24 19:38:47 -0700

    Implement init, getter, and method

--- TimeTracker/CLPTimedTaskController.m
+++ TimeTracker/CLPTimedTaskController.m
@@ -7,7 +7,30 @@
 //
 
 #import "CLPTimedTaskController.h"
+#import "CLPTimedTask.h"
 
 @implementation CLPTimedTaskController
+
+- (instancetype)init
+{
+    if (self = [super init]) {
+        _timedTasks = [[NSMutableArray alloc] init];
+    }
+    return self;
+}
+
+- (NSUInteger)count
+{
+    return self.timedTasks.count;
+}
+
+- (void)createTimedTaskWithClient:(NSString *)client
+                          summary:(NSString *)summary
+                       hourlyRate:(NSDecimalNumber *)hourlyRate
+                      hoursWorked:(NSDecimalNumber *)hoursWorked
+{
+    CLPTimedTask *timedTask = [[CLPTimedTask alloc] initWithClient:client summary:summary hourlyRate:hourlyRate hoursWorked:hoursWorked];
+    [self.timedTasks addObject:timedTask];
+}
 
 @end
