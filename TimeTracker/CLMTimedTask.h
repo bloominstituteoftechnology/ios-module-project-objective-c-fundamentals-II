32b04b7e6efdd16786a65bd497f70ac524cd06bd
Claudia Maciel <clc_80@yahoo.com>
2020-07-24 17:59:10 -0700

    Created new NSObject subclass

---
+++ TimeTracker/CLMTimedTask.h
@@ -0,0 +1,17 @@
+//
+//  CLMTimedTask.h
+//  TimeTracker
+//
+//  Created by Claudia Maciel on 7/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CLMTimedTask : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TimeTracker/CLMTimedTask.m
@@ -0,0 +1,13 @@
+//
+//  CLMTimedTask.m
+//  TimeTracker
+//
+//  Created by Claudia Maciel on 7/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "CLMTimedTask.h"
+
+@implementation CLMTimedTask
+
+@end
