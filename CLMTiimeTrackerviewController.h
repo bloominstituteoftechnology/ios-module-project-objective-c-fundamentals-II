515d8fa83ada0fcd3f0edcdc4f23504ad25a60df
Claudia Maciel <clc_80@yahoo.com>
2020-07-24 18:45:12 -0700

    Adopted the UITableViewDataSource protocol

--- CLMTiimeTrackerviewController.h
+++ CLMTiimeTrackerviewController.h
@@ -7,10 +7,11 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface CLMTiimeTrackerviewController : NSObject
+@interface CLMTiimeTrackerviewController : NSObject <UITableViewDataSource>
 
 @end
 
