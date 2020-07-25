0c7a2ab6c02c3bdf2a9338e1330c61f50528f179
seanacres <6416690+seanacres@users.noreply.github.com>
2020-07-25 14:16:45 -0400

    Created TimeTrackerVC file and adopted UITableViewDataSource protocol

---
+++ TimeTracker/View Controller/SMATimeTrackerViewController.m
@@ -0,0 +1,32 @@
+//
+//  SMATimeTrackerViewController.m
+//  TimeTracker
+//
+//  Created by Sean Acres on 7/25/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "SMATimeTrackerViewController.h"
+
+@interface SMATimeTrackerViewController ()
+
+@end
+
+@implementation SMATimeTrackerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
