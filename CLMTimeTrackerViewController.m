b060f899ca7af71d3b24e47a512cd7f79d1e9091
Claudia Maciel <clc_80@yahoo.com>
2020-07-24 19:14:19 -0700

    Implemented dataSource methods

--- CLMTimeTrackerViewController.m
+++ CLMTimeTrackerViewController.m
@@ -8,6 +8,7 @@
 
 #import "CLMTimeTrackerViewController.h"
 #import "CLMTimedTaskController.h"
+#import "CLMTimedTask.h"
 
 @interface CLMTimeTrackerViewController ()
 
@@ -55,5 +56,21 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+// MARK: - UITableViewDataSource
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.timedTaskController.timedTasks.count;
+}
+
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"taskCell" forIndexPath:indexPath];
+    CLMTimedTask *task = self.timedTaskController.timedTasks[indexPath.row];
+    
+    cell.textLabel.text = task.client;
+    cell.detailTextLabel.text = [NSString localizedStringWithFormat:@"$%.2f", task.total];
+    
+    return cell;
+}
 
 @end
