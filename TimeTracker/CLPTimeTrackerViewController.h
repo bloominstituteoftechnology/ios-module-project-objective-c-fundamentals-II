cef68f3e9d66fc9e0fe785ba4442ee48c24d45cc
Chad Parker <chad@chadparker.com>
2020-07-24 19:41:19 -0700

    Create CLPTimeTrackerViewController

---
+++ TimeTracker/CLPTimeTrackerViewController.h
@@ -0,0 +1,17 @@
+//
+//  CLPTimeTrackerViewController.h
+//  TimeTracker
+//
+//  Created by Chad Parker on 7/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CLPTimeTrackerViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
