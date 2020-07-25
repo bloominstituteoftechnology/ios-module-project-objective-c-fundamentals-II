bb15e479aa91f6fa00ab62ed842f57c0a2b56288
Chad Parker <chad@chadparker.com>
2020-07-24 19:28:54 -0700

    Create CLPTimedTaskController object

---
+++ TimeTracker/CLPTimedTaskController.h
@@ -0,0 +1,17 @@
+//
+//  CLPTimedTaskController.h
+//  TimeTracker
+//
+//  Created by Chad Parker on 7/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CLPTimedTaskController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TimeTracker/CLPTimedTaskController.m
@@ -0,0 +1,13 @@
+//
+//  CLPTimedTaskController.m
+//  TimeTracker
+//
+//  Created by Chad Parker on 7/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "CLPTimedTaskController.h"
+
+@implementation CLPTimedTaskController
+
+@end
