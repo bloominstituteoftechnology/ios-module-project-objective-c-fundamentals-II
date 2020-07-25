be4094e30d99f07dde7780b04b0c6cff66223a72
Chad Parker <chad@chadparker.com>
2020-07-24 19:22:35 -0700

    Add properties in interface, define inititalizer

--- TimeTracker/CLPTimedTask.h
+++ TimeTracker/CLPTimedTask.h
@@ -12,6 +12,17 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface CLPTimedTask : NSObject
 
+@property (nonatomic, copy) NSString *client;
+@property (nonatomic, copy) NSString *summary;
+@property (nonatomic) NSDecimalNumber *hourlyRate;
+@property (nonatomic) NSDecimalNumber *hoursWorked;
+@property (nonatomic, readonly) NSDecimalNumber *total;
+
+- (instancetype)initWithClient:(NSString *)client
+                       summary:(NSString *)summary
+                    hourlyRate:(NSDecimalNumber *)hourlyRate
+                   hoursWorked:(NSDecimalNumber *)hoursWorked;
+
 @end
 
 NS_ASSUME_NONNULL_END
