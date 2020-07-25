e301d71806a396db83a0ff820892da203d28e031
seanacres <6416690+seanacres@users.noreply.github.com>
2020-07-25 14:03:52 -0400

    Created TimedTaskController file

---
+++ TimeTracker/Model Controller/SMATimedTaskController.h
@@ -0,0 +1,17 @@
+//
+//  SMATimedTaskController.h
+//  TimeTracker
+//
+//  Created by Sean Acres on 7/25/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SMATimedTaskController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TimeTracker/Model Controller/SMATimedTaskController.m
@@ -0,0 +1,13 @@
+//
+//  SMATimedTaskController.m
+//  TimeTracker
+//
+//  Created by Sean Acres on 7/25/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "SMATimedTaskController.h"
+
+@implementation SMATimedTaskController
+
+@end
