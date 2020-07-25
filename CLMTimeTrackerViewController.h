9cf2755cf9b092874f4b3cda30948467a6736677
Claudia Maciel <clc_80@yahoo.com>
2020-07-24 18:50:20 -0700

    Created subclass of UIViewController

---
+++ CLMTimeTrackerViewController.h
@@ -0,0 +1,17 @@
+//
+//  CLMTimeTrackerViewController.h
+//  TimeTracker
+//
+//  Created by Claudia Maciel on 7/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CLMTimeTrackerViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CLMTimeTrackerViewController.m
@@ -0,0 +1,32 @@
+//
+//  CLMTimeTrackerViewController.m
+//  TimeTracker
+//
+//  Created by Claudia Maciel on 7/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "CLMTimeTrackerViewController.h"
+
+@interface CLMTimeTrackerViewController ()
+
+@end
+
+@implementation CLMTimeTrackerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
