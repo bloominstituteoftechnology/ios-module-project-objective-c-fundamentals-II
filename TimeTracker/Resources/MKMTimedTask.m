af9803343590b1c5dbf7588b717a38d5d34de1fa
Matt Martindale <martindalematt18@yahoo.com>
2020-07-26 16:09:36 -0600

    Added TimedTask object and initializer

---
+++ TimeTracker/Resources/MKMTimedTask.m
@@ -0,0 +1,13 @@
+//
+//  TimedTask.m
+//  TimeTracker
+//
+//  Created by Matthew Martindale on 7/26/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "MKMTimedTask.h"
+
+@implementation MKMTimedTask
+
+@end
