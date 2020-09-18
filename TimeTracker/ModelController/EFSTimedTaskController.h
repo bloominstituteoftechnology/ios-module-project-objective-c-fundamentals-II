64f2c8040b0eb573900370c370ee91f3de2a45ad
Clayton Watkins <watkinclayton17@yahoo.com>
2020-09-18 11:48:25 -0700

    Created a TimedTaskController with an array and one function in the header

---
+++ TimeTracker/ModelController/EFSTimedTaskController.h
@@ -0,0 +1,23 @@
+//
+//  EFSTimedTaskController.h
+//  TimeTracker
+//
+//  Created by Clayton Watkins on 9/18/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class EFSTimedTask;
+
+@interface EFSTimedTaskController : NSObject
+
+@property (nonatomic) NSMutableArray<EFSTimedTask *> *taskArray;
+
+- (EFSTimedTask *)createTimedTaskWith:(NSString *)aClientName workSummary:(NSString *)aWorkSummary hourlyRate:(double)aHourlyRate hoursWorked:(double)someHoursWorked;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TimeTracker/ModelController/EFSTimedTaskController.m
@@ -0,0 +1,16 @@
+//
+//  EFSTimedTaskController.m
+//  TimeTracker
+//
+//  Created by Clayton Watkins on 9/18/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "EFSTimedTaskController.h"
+
+@implementation EFSTimedTaskController
+
+
+
+
+@end
