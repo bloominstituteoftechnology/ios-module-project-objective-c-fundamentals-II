0d17a0bd7d62c0e416572f6eeeed01b1e102892e
Clayton Watkins <watkinclayton17@yahoo.com>
2020-09-18 12:14:26 -0700

    Created and set up IBOutlets on EFSTimeTrackerViewControler

---
+++ TimeTracker/ViewControllers/EFSTimeTrackerViewController.h
@@ -0,0 +1,17 @@
+//
+//  EFSTimeTrackerViewController.h
+//  TimeTracker
+//
+//  Created by Clayton Watkins on 9/18/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EFSTimeTrackerViewController : UIViewController <UITableViewDataSource>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TimeTracker/ViewControllers/EFSTimeTrackerViewController.m
@@ -0,0 +1,41 @@
+//
+//  EFSTimeTrackerViewController.m
+//  TimeTracker
+//
+//  Created by Clayton Watkins on 9/18/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "EFSTimeTrackerViewController.h"
+#import "EFSTimedTaskController.h"
+#import "EFSTimedTask.h"
+
+@interface EFSTimeTrackerViewController ()
+// Private IBOutlets
+@property (weak, nonatomic) IBOutlet UITextField *clientNameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *workSummaryTextField;
+@property (weak, nonatomic) IBOutlet UITextField *hourlyRateTextField;
+@property (weak, nonatomic) IBOutlet UITextField *timeWorkedTextField;
+
+// Private Properties
+
+@end
+
+@implementation EFSTimeTrackerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+}
+
+@end
