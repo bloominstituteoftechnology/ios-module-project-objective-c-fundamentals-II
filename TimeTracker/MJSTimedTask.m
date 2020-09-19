81f49ff1cadbbc7b09d26849747c2a41903d9be4
morgan smith <mssmitty07@gmail.com>
2020-09-18 21:19:43 -0400

    created model properties

---
+++ TimeTracker/MJSTimedTask.m
@@ -0,0 +1,13 @@
+//
+//  MJSTimedTask.m
+//  TimeTracker
+//
+//  Created by Morgan Smith on 9/18/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "MJSTimedTask.h"
+
+@implementation MJSTimedTask
+
+@end
