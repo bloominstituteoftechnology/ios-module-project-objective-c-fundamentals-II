97e7a8683ab172301b159333a7ac029f4b33d850
brys <brys1saclaus2@gmail.com>
2020-11-14 00:34:19 -0800

    Remade MVC folders + BRSTimedTask Class

---
+++ TimeTracker/BRSTimedTask.h
@@ -0,0 +1,17 @@
+//
+//  BRSTimedTask.h
+//  TimeTracker
+//
+//  Created by BrysonSaclausa on 11/14/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BRSTimedTask : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TimeTracker/BRSTimedTask.m
@@ -0,0 +1,13 @@
+//
+//  BRSTimedTask.m
+//  TimeTracker
+//
+//  Created by BrysonSaclausa on 11/14/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "BRSTimedTask.h"
+
+@implementation BRSTimedTask
+
+@end
