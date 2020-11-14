b8c00d1ae6648606ff4588b9e467e0658a6bbfbb
brys <brys1saclaus2@gmail.com>
2020-11-14 09:26:34 -0800

    defined a standard member wise init for TimedTask object

--- TimeTracker/BRSTimedTask.h
+++ TimeTracker/BRSTimedTask.h
@@ -8,10 +8,33 @@
 
 #import <Foundation/Foundation.h>
 
+
 NS_ASSUME_NONNULL_BEGIN
 
 @interface BRSTimedTask : NSObject
 
+@property (nonatomic, readonly) NSString *clientName;
+@property (nonatomic, readonly) NSString *summaryText;
+@property (nonatomic, readonly) double hourlyRateCharged;
+@property (nonatomic, readonly) double amountOfHoursWorked;
+@property (nonatomic, readonly) double total;
+
+- (instancetype) initWithClientName:(NSString *)aClientName
+                        summaryText:(NSString *)aSummaryText
+                    hourlyRateChanged:(double)aHourlyRateChanged
+                   amountOfHoursWorked:(double)aAmountOfHoursWorked
+                                 total:(double)aTotal;
+                
+
 @end
 
 NS_ASSUME_NONNULL_END
+
+
+/*
+ - The client
+ - The summary of the work you did
+ - The hourly rate charged
+ - The amount of hours worked
+ - The total
+ */
