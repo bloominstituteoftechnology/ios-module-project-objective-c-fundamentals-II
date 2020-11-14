02e84132d8876c37a412cd6563f0de2dcaff8715
Cora Jacobson <cora-jacobson@lambdastudents.com>
2020-11-14 09:58:13 -0800

    Deleted groups and added groups without folders

---
+++ TimeTracker/TimedTaskController.h
@@ -0,0 +1,17 @@
+//
+//  TimedTaskController.h
+//  TimeTracker
+//
+//  Created by Cora Jacobson on 11/14/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TimedTaskController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TimeTracker/TimedTaskController.m
@@ -0,0 +1,13 @@
+//
+//  TimedTaskController.m
+//  TimeTracker
+//
+//  Created by Cora Jacobson on 11/14/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "TimedTaskController.h"
+
+@implementation TimedTaskController
+
+@end
