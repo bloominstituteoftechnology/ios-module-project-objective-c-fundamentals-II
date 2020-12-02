80a032b4ba0864a2b41244e9f40ff2370392bc46
Kenneth Jones <kennethjones1991@outlook.com>
2020-12-02 00:33:22 -0700

    Completed part 3; Completed MVP

---
+++ TimeTracker/MKJTimeTrackerViewController.h
@@ -0,0 +1,17 @@
+//
+//  MKJTimeTrackerViewController.h
+//  TimeTracker
+//
+//  Created by Kenneth Jones on 12/1/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MKJTimeTrackerViewController : UIViewController <UITableViewDataSource>
+
+@end
+
+NS_ASSUME_NONNULL_END
