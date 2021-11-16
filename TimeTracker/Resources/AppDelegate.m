39350c6bc8f94d1ea60bb2e4b1613f785ffc07c7
Bob Lauer <bob@lambdaschool.com>
2021-11-16 16:14:53 -0600

    BloomTech

--- TimeTracker/Resources/AppDelegate.m
+++ TimeTracker/Resources/AppDelegate.m
@@ -3,7 +3,7 @@
 //  TimeTracker
 //
 //  Created by Spencer Curtis on 2/11/20.
-//  Copyright © 2020 Lambda School. All rights reserved.
+//  Copyright © 2020 BloomTech. All rights reserved.
 //
 
 #import "AppDelegate.h"
